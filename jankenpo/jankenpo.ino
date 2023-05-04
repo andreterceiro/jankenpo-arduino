8197d7e3e469b35c19bb39d3f41a4cb39284ef98
André de Paula Terceiro <andre@terceiro.com.br>
2023-05-04 13:32:01 -0300

    Inserting comments in the code and removing commented lines about the serial output

--- jankenpo/jankenpo.ino
+++ jankenpo/jankenpo.ino
@@ -5,18 +5,20 @@ int pin8Value = 0;
 int computerChoice = 0;
 
 void setup() {
+    // First RGB LED - computer choice
     pinMode(0, OUTPUT);
     pinMode(1, OUTPUT);
     pinMode(2, OUTPUT);
 
+    // Second RGB LED - show who is the winner
     pinMode(3, OUTPUT);
     pinMode(4, OUTPUT);
     pinMode(5, OUTPUT);
 
+    // User choice - buttons
     pinMode(6, INPUT);
     pinMode(7, INPUT);
     pinMode(8, INPUT);
-    // Serial.begin(9600);
 }
 
 void loop() {
@@ -26,13 +28,15 @@ void loop() {
     pin7Value = LOW;
     pin8Value = LOW;
 
+    // Waiting the user input
     while (pin6Value == LOW && pin7Value == LOW && pin8Value == LOW) {
+        // Reading the user INPUT
         pin6Value = digitalRead(6);
         pin7Value = digitalRead(7);
         pin8Value = digitalRead(8);
-        // Serial.println("waiting");
     }
     
+    // The 2 LEDs are off in the start
     digitalWrite(0, LOW);
     digitalWrite(1, LOW);
     digitalWrite(2, LOW);
@@ -42,6 +46,7 @@ void loop() {
 
     computerChoice = random(1, 4);
 
+    // Writing the first RGB LED -computer choice
     if (computerChoice == 1) {
         digitalWrite(2, HIGH);
     } else if (computerChoice == 2) {
@@ -50,6 +55,7 @@ void loop() {
         digitalWrite(0, HIGH);
     }
 
+    // Writing who is the winner - the second LED
     if ((computerChoice == 1 && pin8Value == HIGH) || (computerChoice == 2 && pin6Value  == HIGH) || (computerChoice == 3 && pin7Value  == HIGH)) {
         digitalWrite(4, HIGH);
     } else if ((computerChoice == 1 && pin6Value  == HIGH) || (computerChoice == 2 && pin7Value  == HIGH) || (computerChoice == 3 && pin8Value  == HIGH)) {
@@ -62,6 +68,8 @@ void loop() {
 }
 
 void initOutput() {
+    // Before user press a button, we set off all the colors of the first RGB LED
+    // and set white all the colors of the second RGB LED
     digitalWrite(0, LOW);
     digitalWrite(1, LOW);
     digitalWrite(2, LOW);
